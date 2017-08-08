3a04e0c1ef008493a45feaa67560db976df257bc
Jayce77 <xjaycex@hotmail.com>
2017-08-07 23:02:56 -0400

    Bound this to fix error on onInputChange function '

--- WeatherApp/src/containers/search_bar.js
+++ WeatherApp/src/containers/search_bar.js
@@ -5,10 +5,13 @@ export default class SearchBar extends Component {
     super(props);
     
     this.state = {term: ''};
+    
+    this.onInputChange = this.onInputChange.bind(this);
   }
   
   onInputChange(event) {
     console.log(event.target.value);
+    this.setState({ term: event.target.value});
   }
   
   render(){
@@ -18,7 +21,7 @@ export default class SearchBar extends Component {
           placeholder='Get a five day forecast in your favorite cities'
           className='form-control'
           value={this.state.term}
-          onChange={this.onInputChange} />
+          onChange={ this.onInputChange} />
         <span className="input-group-btn">
           <button type='submit' className="btn btn-secondary">Submit</button>
         </span>
