316464c65c273f92366bbe7f7b442c139f62afa0
Jayce77 <xjaycex@hotmail.com>
2017-08-20 22:31:28 -0400

    Wired reducer to WeatherList container

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
-        <tr>
-          <th>City</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
         <tbody></tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
