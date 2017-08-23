b06e5bfa876ffe1a5f4fb97bfb4e01831d4e804d
Jayce77 <xjaycex@hotmail.com>
2017-08-22 22:33:28 -0400

    added addtional charts, google map and styling

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const NAME = cityData.city.name;
     const TEMPS = cityData.list.map(weather => weather.main.temp);
+    const PRESSURES = cityData.list.map(weather => weather.main.pressure);
+    const HUMIDITIES = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       
       <tr key={NAME}>
         <td>
-          {NAME}
+          <GoogleMap lon={lon} lat={lat}/>
         </td>
-        <td>
-          <Chart data={TEMPS} color="orange"/>
-        </td>
+        <td><Chart data={TEMPS} color="orange" units="K"/></td>
+        <td><Chart data={PRESSURES} color="green" units="hPa"/></td>
+        <td><Chart data={HUMIDITIES} color="black" units="%"/></td>
       </tr>
     );
   }
@@ -26,9 +30,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (HPA)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
