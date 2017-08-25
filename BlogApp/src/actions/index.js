58ecaa01834f8342c9d52bcd0f39a5920ad1df4c
Jayce77 <xjaycex@hotmail.com>
2017-08-24 22:55:04 -0400

    Created PostsReducer container

--- BlogApp/src/actions/index.js
+++ BlogApp/src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const FETCH_POSTS 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=balrogtap';
 
 export function fetchPosts() {
-  const request =axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   
   return {
     type: FETCH_POSTS,
