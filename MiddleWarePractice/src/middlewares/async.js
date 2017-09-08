57c7551ffac93d36e6bdb99cc42fc9f244b02553
Jayce77 <xjaycex@hotmail.com>
2017-09-07 22:20:12 -0400

    handled case of payload not containing a promise

--- MiddleWarePractice/src/middlewares/async.js
+++ MiddleWarePractice/src/middlewares/async.js
@@ -1,7 +1,22 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // If the action doesn't have a payload or promie
+    // sent it on to the next middleware
+    if (!action || !action.payload.then) {
+      return next(action);
+    }
     
-    next(action);
+    console.log("We have a promise", action);
   };
 }
+
+/*
+  export default function({dispatch}) {
+    return function(next) {
+      return function(action);
+        console.log(action);
+        
+        next(action);
+    }
+  }
+*/
