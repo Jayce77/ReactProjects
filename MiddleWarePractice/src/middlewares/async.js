08899bf037e95bd15fc6574120d03775d351a6e1
Jayce77 <xjaycex@hotmail.com>
2017-09-07 22:36:14 -0400

    Middleware complete

--- MiddleWarePractice/src/middlewares/async.js
+++ MiddleWarePractice/src/middlewares/async.js
@@ -6,7 +6,15 @@ export default function({ dispatch }) {
       return next(action);
     }
     
-    console.log("We have a promise", action);
+    //Make sure the action's promise resolves
+    action.payload
+      .then(function(response){
+        //Take everything from the current action
+        //Then replace promise with response
+        const newAction = { ...action, payload: response }
+        //dispatch sends action back through all middlewares agian
+        dispatch(newAction);
+    });
   };
 }
 
--- MiddleWarePractice/src/reducers/users.js
+++ MiddleWarePractice/src/reducers/users.js
@@ -6,7 +6,7 @@ export default function(state = [], action){
   switch(action.type){
     case FETCH_USERS:
     //Take the existing state and concatinate on the new list of users
-      return [ ...state, ...action.payload ];
+      return [ ...state, ...action.payload.data ];
   }
   return state;
 }
