11797e676953f629919f52640eb1b09c0b700eba
Jayce77 <xjaycex@hotmail.com>
2017-09-13 22:31:16 -0400

    Added tests for reducer

--- ReduxCounter/src/reducers/reducer_counter.js
+++ ReduxCounter/src/reducers/reducer_counter.js
@@ -1,23 +1,27 @@
 import expect from 'expect';
 
-export default function counter(state, action) {
-  return state;
+export default function CounterReducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  }
 }
 
 expect(
-counter(0, { type: 'INCREMENT' })
+CounterReducer(0, { type: 'INCREMENT' })
 ).toEqual(1);
 
 expect(
-counter(1, { type: 'INCREMENT' })
+CounterReducer(1, { type: 'INCREMENT' })
 ).toEqual(2);
 
 expect(
-counter(2, { type: 'DECREMENT' })
+CounterReducer(2, { type: 'DECREMENT' })
 ).toEqual(1);
 
 expect(
-counter(1, { type: 'INCREMENT' })
+CounterReducer(1, { type: 'DECREMENT' })
 ).toEqual(0);
 
 console.log('Tests passed!');
