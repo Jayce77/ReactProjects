93e7710455a3fefbe6c45b79cc44fa0636c723c9
Jayce77 <xjaycex@hotmail.com>
2017-12-16 22:05:56 -0500

    initial React App

--- indecission-app/public/scripts/app.js
+++ indecission-app/public/scripts/app.js
@@ -2,11 +2,82 @@
 
 console.log('app is running');
 
+var userData = {
+    userName: 'Andrew Mead',
+    userAge: 26,
+    userLocation: 'Philadelphia'
+};
+
+var appData = {
+    appTitle: 'Indecision App',
+    appSubTitile: 'Put you life in the hands of a computer',
+    appOptions: ['List Item 1', 'List Item 2', 'List Item 3']
+};
+
+var getLocation = function getLocation(location) {
+    return location === undefined ? 'Unknown' : location;
+};
+var getOptions = function getOptions(options) {
+    return options.map(function (item, index) {
+        return React.createElement(
+            'li',
+            { key: index },
+            item
+        );
+    });
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'This is new JSX from app.js!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appData.appTitle
+    ),
+    appData.appSubTitle && React.createElement(
+        'p',
+        null,
+        appData.appSubTitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        appData.appInfo
+    ),
+    React.createElement(
+        'p',
+        null,
+        appData.appOptions.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    appData.appOptions && React.createElement(
+        'ol',
+        null,
+        getOptions(appData.appOptions)
+    )
 );
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        userData.userName ? userData.userName : 'Anonymous'
+    ),
+    userData.userAge && userData.userAge >= 18 && React.createElement(
+        'p',
+        null,
+        'age: ',
+        userData.userAge
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        getLocation(userData.userLocation)
+    )
+);
+
 // const template = React.createElement(
 //     "h1",
 //     { id: 'someId' },
--- indecission-app/src/app.js
+++ indecission-app/src/app.js
@@ -1,6 +1,39 @@
 console.log('app is running')
 
-const template =<h1>This is new JSX from app.js!</h1>
+const userData = {
+    userName: 'Andrew Mead',
+    userAge: 26,
+    userLocation: 'Philadelphia'
+}
+
+const appData = {
+    appTitle: 'Indecision App',
+    appSubTitile: 'Put you life in the hands of a computer',
+    appOptions: ['List Item 1','List Item 2', 'List Item 3']
+}
+
+const getLocation = (location) => location === undefined ? 'Unknown' : location
+const getOptions = (options) => options.map((item, index) => <li key={index}>{item}</li>)
+
+const template = (
+    <div>
+        <h1>{appData.appTitle}</h1>
+        { appData.appSubTitle && <p>{appData.appSubTitle}</p> }
+        <p>{appData.appInfo}</p>
+        <p>{ appData.appOptions.length > 0 ? 'Here are your options' : 'No options' }</p>
+        {appData.appOptions && <ol>
+            { getOptions(appData.appOptions) }
+        </ol>}
+    </div>
+)
+const templateTwo = (
+    <div>
+        <h1>{userData.userName ? userData.userName : 'Anonymous'}</h1>
+        {userData.userAge && userData.userAge >= 18 && <p>age: {userData.userAge}</p>}
+        <p>Location: {getLocation(userData.userLocation)}</p>
+    </div>
+)
+
 // const template = React.createElement(
 //     "h1",
 //     { id: 'someId' },
