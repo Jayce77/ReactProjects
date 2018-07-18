905d1353a8c1c570ca9eb5290c7a262cc3a84028
Jayce77 <xjaycex@hotmail.com>
2018-07-18 16:43:02 -0400

    added settings and container scss files

--- indecission-app/src/components/IndecisionApp.js
+++ indecission-app/src/components/IndecisionApp.js
@@ -78,22 +78,24 @@ class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle}/>
-        <Action 
-          hasOptions={this.state.options.lenth > 0 ? false : true}
-          handlePick={this.handlePick}
-        />
-        <Options 
-          options={this.state.options}
-          handleRemoveAll={this.handleRemoveAll}
-          handleRemoveSingleOption={this.handleRemoveSingleOption} />
-        <AddOption 
-          handleAddOption={this.handleAddOption} 
-          newOption={this.state.newOption}
-        />
-        <OptionModal 
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
+        <div className='container'>
+          <Action 
+            hasOptions={this.state.options.lenth > 0 ? false : true}
+            handlePick={this.handlePick}
+          />
+          <Options 
+            options={this.state.options}
+            handleRemoveAll={this.handleRemoveAll}
+            handleRemoveSingleOption={this.handleRemoveSingleOption} />
+          <AddOption 
+            handleAddOption={this.handleAddOption} 
+            newOption={this.state.newOption}
+          />
+          <OptionModal 
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
+        </div>
       </div>
     )
   }
