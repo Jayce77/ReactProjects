f4d03152c80294f9bac92ece15d1aaa7cd1a18fd
Jayce77 <xjaycex@hotmail.com>
2018-07-21 16:48:15 -0400

    HOC practice

---
+++ expensify-app/src/playground/hoc.js
@@ -0,0 +1,43 @@
+//Higher Order Components (HOC) - a compnent that renders another component
+//Reuse code
+//Render hijacking
+//Prop mnanipulation
+//Abstract State
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+)
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is private info. Please don't share</p> }
+      <WrappedComponent {...props}/>
+    </div>
+  )
+}
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenicated 
+        ? <WrappedComponent {...props}/>
+        : <p>Please log in to view the info</p>
+      }
+    </div>
+  )
+}
+
+
+
+const AdminInfo = withAdminWarning(Info)
+const AuthInfo = requireAuthentication(Info)
+
+ReactDOM.render(<AdminInfo isAdmin={true} info='These are the details' />, document.getElementById('app'))
+ReactDOM.render(<AuthInfo isAuthenicated={false} info='These are the details' />, document.getElementById('app'))
