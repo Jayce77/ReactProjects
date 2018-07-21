b22f5a42b7bb8c433321cf887a1e8537dcb220b3
Jayce77 <xjaycex@hotmail.com>
2018-07-20 20:08:01 -0400

    Added filtersReducer and expensesReducer to redux-expensify

---
+++ expensify-app/src/playground/redux-expensify.js
@@ -0,0 +1,51 @@
+import { createStore, combineReducers } from 'redux'
+
+
+const expensesReducerDefaultState = []
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type){
+    default:
+      return state
+  }
+}
+
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) =>{
+  switch (action.type){
+    default:
+      return state
+  }
+}
+
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  }), /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+console.log(store.getState())
+
+const demoState = {
+  expenses: [{
+    id: '123sded',
+    description: 'January rent',
+    note: 'This was the final payment for that address',
+    amount: 54500,
+    createdAt: 0 
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount', // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+}
+
