f945822059eb2c404d759fe100fde7d920ad8624
Jayce77 <xjaycex@hotmail.com>
2018-07-29 13:39:05 -0400

    Moved remove button to Edit page

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -2,16 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import ExpenseForm from './ExpenseForm'
-import { editExpense } from '../actions/expenses'
+import { editExpense, removeExpense } from '../actions/expenses'
 
 const EditExpensePage = (props) => (
-  <ExpenseForm 
-    onSubmit={(expense) => {
-      props.dispatch(editExpense(props.expense.id, expense))
-      props.history.push('/')
-    }}
-    expense={props.expense}
-  />
+  <div>
+    <ExpenseForm 
+      expense={props.expense}
+      onSubmit={(expense) => {
+        props.dispatch(editExpense(props.expense.id, expense))
+        props.history.push('/')
+      }}
+    />
+    <button 
+      onClick={() => {
+        props.dispatch(removeExpense({id: props.expense.id}))
+        props.history.push('/')
+      }} 
+    >
+      Remove
+    </button>
+  </div>
 )
 
 const mapStateToProps = (state, props) => {
